44e1bbfe43390b81b1035327dffa51ca32baeb4b
bcattaneo <c@ttaneo.uy>
2023-03-13 21:50:49 -0300

    feat: improve example

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,11 @@
 import 'package:qr_terminal/qr_terminal.dart' as $qr_terminal;
 
 void main(List<String> arguments) {
+  // Generate a QR
   $qr_terminal.generate("Hello World!");
+
+  print("");
+
+  // Generate a QR specifying a QRCode type
+  $qr_terminal.generate("Smaller", typeNumber: 4);
 }
