e9a247564a52ee1d03c2fb13fb86cce98e773455
bcattaneo <c@ttaneo.uy>
2023-03-21 17:51:31 -0300

    feat: refactor QR generator and add small generation'

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,19 @@
 import 'package:qr_terminal/qr_terminal.dart' as $qr_terminal;
 
 void main(List<String> arguments) {
-  // Generate a QR
+  // Generate a regular QR
   $qr_terminal.generate("Hello World!");
+  print("");
 
+  // Generate a small QR
+  $qr_terminal.generate("Hello World!", small: true);
   print("");
 
   // Generate a QR specifying a QRCode type
-  $qr_terminal.generate("Smaller", typeNumber: 4);
+  $qr_terminal.generate("Smol", typeNumber: 4);
+  print("");
+
+  // Generate a QR specifying a QRCode type and make it small
+  $qr_terminal.generate("Smoller", typeNumber: 4, small: true);
+  print("");
 }
